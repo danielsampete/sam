9d50b63412dd386bcac15acf19d073eaca57b1f6
danielsampete <danielsam.pete@chronus.com>
2014-06-13 11:15:00 +0530

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :correct_user,   only: [:edit, :update]
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :admin_user,     only: :destroy
   def index
     @users = User.paginate(page: params[:page])
@@ -40,7 +41,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
